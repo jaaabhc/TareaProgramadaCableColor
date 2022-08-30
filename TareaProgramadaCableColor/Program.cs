e9cc1ebb26dde7722efca786416d170b8626e088
jaaabhc <jaguilar@banhcafe.hn>
2022-08-30 10:33:32 -0600

    Envio de Correo

--- TareaProgramadaCableColor/Program.cs
+++ TareaProgramadaCableColor/Program.cs
@@ -12,15 +12,28 @@ namespace TareaProgramadaCableColor
     {
         static void Main(string[] args)
         {
-            var listaPagos = ListapPagosCableColor();
-            int pagosrealizados = 0;
-            pagosrealizados=listaPagos.Count;
+            try
+            {
+                var listaPagos = ListapPagosCableColor();
+                int pagosrealizados = 0;
+                pagosrealizados = listaPagos.Count;
 
-            var monto = pagosrealizados * 10;
 
-          var contabilizar= hacerContabilidad("2614000414", "5299927000000000", "2110101030500000", "COBRO COMISION CABLE COLOR", "COBRO COMISION CABLE COLOR", monto.ToString(),"2","0","APLICATIVO",0,0,"LPS","01");
 
-            Console.ReadLine();
+                if (pagosrealizados > 0)
+                {
+                    var monto = pagosrealizados * 10;
+                    var contabilizar = hacerContabilidad("2614000414", "5299927000000000", "2110101030500000", "COBRO COMISION CABLE COLOR", "COBRO COMISION CABLE COLOR", monto.ToString(), "2", "0", "APLICATIVO", 0, 0, "LPS", "01");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
 
         }
 
@@ -29,7 +42,12 @@ namespace TareaProgramadaCableColor
         {
             AS400 con = new AS400();
             con.abrirConexion();
-            OleDbDataReader reader = con.consultar("SELECT * FROM BHCCYFILES.AUDHI WHERE AUDTCD=5711 AND AUDFLG='A' AND AUDDTM=8");
+            var fecha = DateTime.Now.AddDays(-1);
+            var dia = fecha.ToString("dd");
+            var mes = fecha.ToString("MM");
+            var año = fecha.ToString("yyyy");
+
+            OleDbDataReader reader = con.consultar("SELECT * FROM BHCCYFILES.AUDHI WHERE AUDTCD=5711 AND AUDFLG='A' AND AUDDTM=" + mes + " AND AUDDTD=" + dia + " AND AUDDTY=" + año);
             List<AUDHI> listaDOCTIP = new List<AUDHI>();
             try
             {
@@ -72,9 +90,41 @@ namespace TareaProgramadaCableColor
             return listaDOCTIP;
         }
 
+        public static void Enviar_Correo(string contenido, string destinatario, string asunto)
+        {
+            System.Net.Mail.MailMessage correo = new System.Net.Mail.MailMessage();
+
+
+            //Destino de correo y su contenido
+            correo.From = new System.Net.Mail.MailAddress("no-reply@banhcafe.bhc");
+            correo.Subject = asunto;
+            correo.To.Add(destinatario);
+            correo.IsBodyHtml = true;
+            correo.Body = contenido;
+            correo.Priority = System.Net.Mail.MailPriority.High;
+
+            var Servidor = new System.Net.Mail.SmtpClient();
+            Servidor.Host = "correo.banhcafe.hn";
+
+
+            Servidor.Credentials = new System.Net.NetworkCredential("no-reply@banhcafe.bhc", "BHCgen2017");
+            try
+            {
+                Console.WriteLine("Enviando correo");
+                Servidor.Send(correo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+
+
+        }
+
 
         //Hacer contabilidad Directamente en el TRANS
-        public static bool hacerContabilidad(string cuentaDebito, string cuentaCredito,string cuentaDetalle, string descripcionCredito, string descripcionDebito, string monto, string agencia, string referencia, string usuario, int id, int? env, string moneda, string banco)
+        public static bool hacerContabilidad(string cuentaDebito, string cuentaCredito, string cuentaDetalle, string descripcionCredito, string descripcionDebito, string monto, string agencia, string referencia, string usuario, int id, int? env, string moneda, string banco)
         {
             AS400 con = new AS400();
             con.abrirConexion();
@@ -88,17 +138,17 @@ namespace TareaProgramadaCableColor
                 var mes = i.ToString("MM");
                 var año = i.ToString("yyyy");
 
-             
 
-              
-                    var sql2 = "INSERT INTO BHCCYFILES.TRANS (TRABTH, TRAVDM, TRAVDD, TRAVDY, TRABNK, TRABRN, TRACCY, TRAGLN, TRAACC, TRABDM, TRABDD, TRABDY, TRACDE, TRANAR, TRAAMT, TRADCC, TRAUSA, TRAUSO, TRAEXR, TRADRR, TRAPTS, TRAAPC, TRAREF, TRANID, TRATRN, TRAPMN, TRASEQ, TRACKN, TRACCN, TRACUN, TRAMOD, TRAOBK, TRAOBR, TRAAAF, TRAEQV, TRAACR, TRACNU, TRARCL, TRACNL, TRADED, TRADSQ, TRAIVP, TRAIVB, TRAREV, TRAOLF, TRACOD, TRATMS)" +
-                                " VALUES(8002, " + mes + ", " + dia + ", " + año + ", '" + banco + "', " + agencia + ", '" + moneda + "', " + cuentaCredito + ",0, " + mes + ", " + dia + ", " + año + ", 'MC', '" + descripcionCredito + "', " + monto + ", '5', '" + usuario + "', '" + usuario + "', 1.000000, 0, 0, '  ', '                         ', 0, 0, 0, 2, 0, 0, 336409, '1',  '" + banco + "', 1, ' ', 0.00, 0, 0, '                    ', '$1', '    ', 0, 0.000, 0.00, ' ', ' ', '    ', CURRENT_TIMESTAMP)";
-                    con.ejecutar(sql2);
 
-                    var sqlED = "INSERT INTO BHCCYFILES.TRANS (TRABTH, TRAVDM, TRAVDD, TRAVDY, TRABNK, TRABRN, TRACCY, TRAGLN, TRAACC, TRABDM, TRABDD, TRABDY, TRACDE, TRANAR, TRAAMT, TRADCC, TRAUSA, TRAUSO, TRAEXR, TRADRR, TRAPTS, TRAAPC, TRAREF, TRANID, TRATRN, TRAPMN, TRASEQ, TRACKN, TRACCN, TRACUN, TRAMOD, TRAOBK, TRAOBR, TRAAAF, TRAEQV, TRAACR, TRACNU, TRARCL, TRACNL, TRADED, TRADSQ, TRAIVP, TRAIVB, TRAREV, TRAOLF, TRACOD, TRATMS)" +
-                                        " VALUES(8002, " + mes + ", " + dia + ", " + año + ", '" + banco + "', " + agencia + ", '" + moneda + "', " + cuentaDetalle + ", " + cuentaDebito + ", " + mes + ", " + dia + ", " + año + ", 'MD', '" + descripcionDebito + "', " + monto + ", '0', '" + usuario + "', '" + usuario + "', 1.000000, 0, 0, '  ', '                         ', 0, 0, 0, 2, 0, 0, 336409, '1',  '" + banco + "', 1, ' ', 0.00, 0, 0, '                    ', '$1', '    ', 0, 0.000, 0.00, ' ', ' ', '    ', CURRENT_TIMESTAMP)";
-                    con.ejecutar(sqlED);
-                
+
+                var sql2 = "INSERT INTO BHCCYFILES.TRANS (TRABTH, TRAVDM, TRAVDD, TRAVDY, TRABNK, TRABRN, TRACCY, TRAGLN, TRAACC, TRABDM, TRABDD, TRABDY, TRACDE, TRANAR, TRAAMT, TRADCC, TRAUSA, TRAUSO, TRAEXR, TRADRR, TRAPTS, TRAAPC, TRAREF, TRANID, TRATRN, TRAPMN, TRASEQ, TRACKN, TRACCN, TRACUN, TRAMOD, TRAOBK, TRAOBR, TRAAAF, TRAEQV, TRAACR, TRACNU, TRARCL, TRACNL, TRADED, TRADSQ, TRAIVP, TRAIVB, TRAREV, TRAOLF, TRACOD, TRATMS)" +
+                            " VALUES(8002, " + mes + ", " + dia + ", " + año + ", '" + banco + "', " + agencia + ", '" + moneda + "', " + cuentaCredito + ",0, " + mes + ", " + dia + ", " + año + ", 'MC', '" + descripcionCredito + "', " + monto + ", '5', '" + usuario + "', '" + usuario + "', 1.000000, 0, 0, '  ', '                         ', 0, 0, 0, 2, 0, 0, 336409, '1',  '" + banco + "', 1, ' ', 0.00, 0, 0, '                    ', '$1', '    ', 0, 0.000, 0.00, ' ', ' ', '    ', CURRENT_TIMESTAMP)";
+                con.ejecutar(sql2);
+
+                var sqlED = "INSERT INTO BHCCYFILES.TRANS (TRABTH, TRAVDM, TRAVDD, TRAVDY, TRABNK, TRABRN, TRACCY, TRAGLN, TRAACC, TRABDM, TRABDD, TRABDY, TRACDE, TRANAR, TRAAMT, TRADCC, TRAUSA, TRAUSO, TRAEXR, TRADRR, TRAPTS, TRAAPC, TRAREF, TRANID, TRATRN, TRAPMN, TRASEQ, TRACKN, TRACCN, TRACUN, TRAMOD, TRAOBK, TRAOBR, TRAAAF, TRAEQV, TRAACR, TRACNU, TRARCL, TRACNL, TRADED, TRADSQ, TRAIVP, TRAIVB, TRAREV, TRAOLF, TRACOD, TRATMS)" +
+                                    " VALUES(8002, " + mes + ", " + dia + ", " + año + ", '" + banco + "', " + agencia + ", '" + moneda + "', " + cuentaDetalle + ", " + cuentaDebito + ", " + mes + ", " + dia + ", " + año + ", 'MD', '" + descripcionDebito + "', " + monto + ", '0', '" + usuario + "', '" + usuario + "', 1.000000, 0, 0, '  ', '                         ', 0, 0, 0, 2, 0, 0, 336409, '1',  '" + banco + "', 1, ' ', 0.00, 0, 0, '                    ', '$1', '    ', 0, 0.000, 0.00, ' ', ' ', '    ', CURRENT_TIMESTAMP)";
+                con.ejecutar(sqlED);
+
 
 
 
